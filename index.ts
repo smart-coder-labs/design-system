faf7f8248d994a65fd7566fc74c1da10c0bdcf85
cesar <cr8297408@gmail.com>
2026-02-21 17:38:30 -0500

    remove test cyberpunks

--- index.ts
+++ index.ts
@@ -15,15 +15,3 @@ export * from './components/ui/UnscramblingText';
 export * from './components/ui/FloatingElement';
 export * from './components/ui/ComicPanel';
 export * from './components/ui/ParallaxStoryStage';
-
-export * from './components/ui/KineticMarquee';
-export * from './components/ui/PowerMeterTracker';
-export * from './components/ui/MagneticButton';
-export * from './components/ui/HolographicCard';
-export * from './components/ui/TerminalAccordion';
-export * from './components/ui/WebGLHologram';
-export * from './components/ui/WebGLDistortionImage';
-export * from './components/ui/WebGLTunnel';
-export * from './components/ui/SystemBootPreloader';
-export * from './components/ui/CRTOverlay';
-export * from './components/ui/GlitchPageTransition';
