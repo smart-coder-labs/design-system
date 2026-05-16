e74acca1f04240a3c88d27ef34dda690eefc593b
Cesar <cr8297408@gmail.com>
2026-05-15 22:22:18 -0500

    refactor: convert select and data components to compound pattern
    - Select: extract trigger, content, item, group sub-components
    - Combobox: create compound structure
    - CommandMenu: create compound structure
    - DataGrid: create compound structure with column/row types

---
+++ components/ui/Select/index.ts
@@ -0,0 +1,2 @@
+export { Select, SelectTrigger, SelectContent, SelectItem, SelectLabel, SelectSeparator } from './Select';
+export type { SelectProps, SelectTriggerProps, SelectContentProps, SelectItemProps, SelectLabelProps, SelectSeparatorProps } from './Select.types';
